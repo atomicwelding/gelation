5b67a16056b71f3aa0451efa293e3c82ee83732d
atomicwelding <ctfweld@gmail.com>
2024-12-24 11:40:18 +0100

    first commit

---
+++ visualize.py
@@ -0,0 +1,9 @@
+###############################################################################
+# Project: gelation/visualize.py
+# Author: Erwan Le Doeuff (weld)
+#
+# Description: Visualize bond percolation lattices.
+###############################################################################
+
+import matplotlib.pyplot as plt
+import numpy as np
